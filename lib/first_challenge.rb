790b5e906665147652f74acc674a6956cdb66268
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-26 15:45:47 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,6 @@
 def first_challenge
    epic_tragedy = {}
-   epic_tragedy["montague"] = ""
-   epic_tragedy["capulet"] = ""
+   epic_tragedy[""] = "montague"
+   epic_tragedy[""] = "capulet"
    return epic_tragedy
 end
